374c3801ef76a69d672e9d1a13e1b9ee2c15f58c
Mai El-Awini <mai.elawini@gmail.com>
2015-04-24 23:06:43 -0300

    radial memu

--- include/js/gallery.js
+++ include/js/gallery.js
@@ -37,7 +37,7 @@ $(document).ready(function () {
     
     //construct gallery items
     
-    var $gallery = $('.pure-g.fullscreen-g');
+    var $gallery1 = $('#slide1 .pure-g.fullscreen-g#work-portfolio');
     for(var i=0; i<images.length/2; i++) {
         var $new_item = $('<div/>', {
             'class':'l-box pure-u-1 pure-u-md-1-2 pure-u-lg-1-4',
@@ -54,7 +54,27 @@ $(document).ready(function () {
                             + "</div>"
                         + "</div>"
                     + "</div>"
-        }).appendTo($gallery);
+        }).appendTo($gallery1);
+    }
+    var $gallery2 = $('#slide2 .pure-g.fullscreen-g#work-portfolio');
+    for(var i=images.length/2; i<images.length; i++) {
+        console.log('item' + i);
+        var $new_item = $('<div/>', {
+            'class':'l-box pure-u-1 pure-u-md-1-2 pure-u-lg-1-4',
+            'html': "<div class='item'>"
+                        + "<div class='face' style='background-image:url("+images[i]+")'></div>"
+                        + "<a class='expand-button' href=''><i class='fa fa-link'></i></a>"
+                        + "<div class='curtain'>"
+                            + "<div class='content-subhead-wrapper'>"
+                                + "<div class='content-subhead'>"
+                                    + "<h3 class='title'>"+titles[i]+"</h3>"
+                                    + "<h2 class='dash'>___</h2>"
+                                    + "<h4 class='inner'>"+sub_titles[i]+"</h4>"
+                                + "</div>"
+                            + "</div>"
+                        + "</div>"
+                    + "</div>"
+        }).appendTo($gallery2);
     }
 
 });
@@ -62,6 +82,13 @@ $(document).ready(function () {
 /************** SPLASH MENU ANIMATIONS ****************/
 
 function ShowSplash(index, length) { 
+    // $('.splash-container .overlay').animate({
+    //     backgroundColor: 'rgba(0,0,0,0.95)'
+    // }, 300);
+
+    
+
+
     $('.splash .splash-item').eq(index).animate({
         top: '0',
         opacity: '1',
@@ -72,11 +99,19 @@ function ShowSplash(index, length) {
     }, 80);
 }
 
-function ToggleTopMenu() { 
-   $('.menu-item').fadeToggle('slow');
-}
-
-function HideSplash() {     
+function HideSplash() {  
+    // $('.splash-container .overlay').animate({
+    //     backgroundColor: 'rgba(0,0,0,0.95)'
+    // }, 300);
+    $('.menu').animate({
+        top: '-200px'
+    },300);
+    setTimeout(function(){
+        $('.menu').css('left', '50px');
+        $('.menu').animate({
+            top: '15px'
+        },200)}, 1000);
+    $('.menu').addClass('side-menu');   
     $('.footer').css('visibility', 'hidden');     
     // $('.splash-container').animate({
     //     width: '150px'
@@ -91,10 +126,60 @@ function HideSplash() {
     }, 100);
 }
 
+/************* Menu Animations *********************/
+
+function ToggleMenuTab() { 
+   $('.menu-item').fadeToggle('slow');
+}
+
+function ToggleMenu() {
+    if($('.menu').hasClass('open')) {
+        $('.menu').removeClass('open');
+        HideMenu($('.menu .splash-item').length);
+    }
+    else{
+        $('.menu').addClass('open');
+        ShowMenu(0, $('.menu .splash-item').length);
+    }
+}
+
+function ShowMenu(index, length) { 
+    if(index == 0) {
+        $('.splash-logo').animate({
+            width: '80px',
+            height: '80px'
+        },100);
+    }
+    $('.menu .splash-item').eq(index).addClass('slideout');
+    setTimeout(function () {   
+        if (++index < length) ShowMenu(index, length);
+    }, 40);
+}
+
+function HideMenu(index) {
+    if(index == $('.menu .splash-item').length){
+        $('.splash-logo').animate({
+            width: '70px',
+            height: '70px'
+        },100);
+    }
+    $('.menu .splash-item').eq(index).removeClass('slideout');
+    setTimeout(function () {   
+        if (--index >= 0) HideMenu(index);
+    }, 40);
+}
+
+function HideMenuInstant() {
+    $('.splash-logo').css('width', '70px');
+    $('.splash-logo').css('height', '70px');
+    $('.menu .splash-item').removeClass('slideout');
+    $('.menu').removeClass('open');
+}
+
 /************** PORTFOLIO ANIMATIONS ****************/
 
 function ShowPortfolioFromTop(index, length) {    
-    $('.pure-g .l-box').eq(index).animate({
+    $('.pure-g#work-portfolio .l-box').eq(index).animate({
         top: 0,
         opacity: 1
     },500);
@@ -103,7 +188,7 @@ function ShowPortfolioFromTop(index, length) {
     }, 40);
 }
 function ShowPortfolioFromBottom(index) {    
-    $('.pure-g .l-box').eq(index).animate({
+    $('.pure-g#work-portfolio .l-box').eq(index).animate({
         top: 0,
         opacity: 1
     },500);
@@ -113,7 +198,7 @@ function ShowPortfolioFromBottom(index) {
 }
 
 function HidePortfolio() {    
-    $( ".pure-g .l-box" ).each(function( index ) {
+    $( ".pure-g#work-portfolio .l-box" ).each(function( index ) {
         $(this).animate({
             opacity: 0
         },500);
@@ -123,10 +208,10 @@ function HidePortfolio() {
 
 //bind buttons
 
-var firstClick = true;
 $('.splash-item').click(function(event) {
     event.stopPropagation();
-    $('.menu ul.splash-item-list').slideUp(300);
+    HideMenu(0, $('.menu .splash-item').length);
+
 });
 
 // $('.splash-item#work').click(function(event) {
@@ -149,7 +234,8 @@ $('.splash-item').click(function(event) {
 
 $('.splash-logo').click(function(event) {
     if($('.menu').hasClass('side-menu')) {
-        $('.menu ul.splash-item-list').slideToggle(300);
+        // $('.menu ul.splash-item-list').slideToggle(300);
+        ToggleMenu();
     }
 });
 
