adbfdd44ad317d16fd673d795e7ebdea1f205e5a
Mai El-Awini <mai.elawini@gmail.com>
2015-06-07 16:14:13 -0300

    image update

--- include/js/main.js
+++ include/js/main.js
@@ -2,18 +2,12 @@ $(document).ready(function() {
 
 	/************** SPLASH BUTTONS ****************/
 
-	$('.splash-item#about').click(function(event) {
-        event.stopPropagation();
-        $('#fullpage').fullpage.moveTo(2);
-    });
+	
     $('.splash .theme-button').click(function(event) {
         event.stopPropagation();
         $('#fullpage').fullpage.moveTo(2);
     });
-    $('.splash-item#contact').click(function(event) {
-        event.stopPropagation();
-        $('#fullpage').fullpage.moveTo(4);
-    });
+    
 
 
     /************** HOME BUTTON ****************/
